3e5a56d304eb5442ebad5674c50e8bfb4e777f61
Gabrielle 🌿 <gabrielle@GabrielesLaptop.fios-router.home>
2024-01-06 10:42:38 -0500

    polished up the buttons on the main page, linked the api to brewery by city search, cleaned up html, css, and script.js files, and fixed bg img for mobile devices

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,36 +1,39 @@
-async function searchBreweries() {
-  const searchType = document.getElementById('search-type').value;
-  const searchInput = document.getElementById('search-input').value;
-  const resultsContainer = document.getElementById('results-container');
-  resultsContainer.innerHTML = '';
+const citySearchBtn = document.getElementById('city-search-form')
 
-  let apiUrl = '';
+function searchBreweriesByCity(event) {
+    event.preventDefault();
+    // capture the user input
+    let cityInput = document.getElementById('city-input');
 
-  if (searchType === 'city') {
-      apiUrl = `https://api.openbrewerydb.org/breweries?by_city=${searchInput}`;
-  } else if (searchType === 'zip') {
-      apiUrl = `https://api.openbrewerydb.org/breweries?by_postal=${searchInput}`;
-  } else if (searchType === 'random') {
-      apiUrl = 'https://api.openbrewerydb.org/breweries/random';
-  }
+    // the user input will then be added to the api url
+    let apiUrl = 'https://api.openbrewerydb.org/breweries?by_city=' + cityInput.value;
 
-  try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
 
-      if (data.length === 0) {
-          resultsContainer.innerHTML = '<p>No breweries found.</p>';
-      } else {
-          data.forEach(brewery => {
-              resultsContainer.innerHTML += `<div>
-                  <h3>${brewery.name}</h3>
-                  <p>${brewery.street}, ${brewery.city}, ${brewery.state} ${brewery.postal_code}</p>
-                  <p>Type: ${brewery.brewery_type}</p>
-              </div>`;
-          });
-      }
-  } catch (error) {
-      resultsContainer.innerHTML = '<p>Something went wrong. Please try again later.</p>';
-  }
+    // fetch the data from the api. once it returns the inital response. we will need to JSON the data. Once the Data is converted we can then use it to add content to the page
+    fetch(apiUrl).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        console.log(data);
+
+        localStorage.setItem('cityBreweryData', JSON.stringify(data))
+
+        window.location.href = './assets/results.html'
+    })
+
+   
+
+    //   if (searchType === 'city') {
+    //       apiUrl = `https://api.openbrewerydb.org/breweries?by_city=${searchInput}`;
+    //   } else if (searchType === 'zip') {
+    //       apiUrl = `https://api.openbrewerydb.org/breweries?by_postal=${searchInput}`;
+    //   } else if (searchType === 'random') {
+    //       apiUrl = 'https://api.openbrewerydb.org/breweries/random';
+    //   }
+
+
 }
+
+citySearchBtn.addEventListener('submit', searchBreweriesByCity)
+
+
 
